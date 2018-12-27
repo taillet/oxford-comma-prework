9ae561fee78f8726f274edd586d8eb2619a8beae
Heloise Taillet <ht2394@columbia.edu>
2018-12-27 00:17:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ if array.size == 1
   return array[0]
 elsif array.size == 2
   return array.join(" and ")
-elsif array.size == 3
+elsif array.size >= 3
   string = array.pop
   last = "and " + string
   array << last
-  return array.join(", ")
+  return array.join(", ")  
   end
 end
