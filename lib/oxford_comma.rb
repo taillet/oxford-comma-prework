058895383eef5789112798056295dabf9aa3d5fa
Heloise Taillet <ht2394@columbia.edu>
2018-12-27 00:07:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ if array.size == 1
 elsif array.size == 2
   return array.join(" and ")
 elsif array.size == 3
-  array.insert(3, "and")
+  array.insert(2, "and")
   return array.join(",")
-  
+
   end
 end
