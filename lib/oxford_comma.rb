dcfc45db1dbae1f4e5fcb6e3afe3197ab21547c6
Heloise Taillet <ht2394@columbia.edu>
2018-12-27 00:18:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.size == 1
+  return array[0]
+elsif array.size == 2
+  return array.join(" and ")
+elsif array.size >= 3
+  string = array.pop
+  last = "and " + string
+  array << last
+  return array.join(", ")
+  end
 end
